47a687701fb47658fbf259b314c1074f3225778f
Lucas Donck <Lucas Donck>
2017-05-08 15:51:03 +0200

    Lucas is the best

---
+++ src/nl/kadaster/oca/MyFirstOCAProgram.java
@@ -0,0 +1,21 @@
+package nl.kadaster.oca;
+
+public class MyFirstOCAProgram {
+
+  public static void main(String[] args) {
+    System.out.println("Mijn eerste OCA programma");
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  }
+
+}
