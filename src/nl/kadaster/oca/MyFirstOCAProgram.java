d4e5f5cdeb778de245eaffed54533ad026920e08
Lucas Donck <Lucas Donck>
2017-05-08 15:53:47 +0200

    Een toevoeging

--- src/nl/kadaster/oca/MyFirstOCAProgram.java
+++ src/nl/kadaster/oca/MyFirstOCAProgram.java
@@ -4,7 +4,7 @@ public class MyFirstOCAProgram {
 
   public static void main(String[] args) {
     System.out.println("Mijn eerste OCA programma");
-
+    System.out.println("Een toevoegging die ik noet zo heel nuttig vind.");
     
     
     
